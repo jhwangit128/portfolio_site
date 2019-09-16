8f91b9c03c58200f2a381b47a50b29751f165db6
jhwangit128 <peonyanko128@gmail.com>
2019-09-15 19:08:20 -0500

    modal open and close

--- app.js
+++ app.js
@@ -1,34 +1,15 @@
-// portfolio modals
-// $(() => {
-//
- //  $('.modal_open_button').on('click', () => {
- //   let openThis = $(this).find('.modal_content');
- //   $(openThis).css("display", "block");
- //   $("body").css("overflow", "hidden");
- // });
-//
-//   $('.close_button').on('click', () => {
-//     let closeThis = $(this).parent('div').fadeOut()
-//     $('body').css('overflow', 'scroll')
-//   })
-//
-// })
+// PROJECT MODALS
 
 $(() => {
-
-  $('.modal_open_button').on('click', () => {
-   let openThis = $(this).find('.modal_content');
-   $(openThis).css("display", "block");
-   $("body").css("overflow", "hidden");
- });
-  // $('.modal_open_button').on('click', function() {
-  //  var openThis = $(this).find('.modal_content');
-  //  $(openThis).css("display", "block");
-  //  $("body").css("overflow", "hidden");
-  // });
-
+  // Open Modal
+  $('.modal_open_button').on('click', function() {
+    let openThis = $(this).find('.modal_content');
+    $(openThis).css("display", "block");
+    $("body").css("overflow", "hidden");
+});
+  // Close Modal
   $('.close_button').on('click', function() {
-    var closeThis = $(this).parent('div').fadeOut();
+    let closeThis = $(this).parent('div').fadeOut();
     $('body').css('overflow', 'scroll')
   });
 
